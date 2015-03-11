e486a0c3f5b3f90bd4365b2dc700413c7fce1d53
vagrant <dverhas@gmail.com>
2015-03-11 02:00:05 +0000

    Move development info to the wiki and update vagrant_setup to include selendium and chromium-browser installs.

--- README.md
+++ README.md
@@ -2,19 +2,6 @@
 http://frozensquirrels.pythonanywhere.com
 
 ## Development 
-If your developing on a Vagrant instance run:
 
-```
-sudo apt-get install git
-cd /vagrant
-git clone git@github.com:FrozenSquirrels/polyglot.git
-cd polyglot
-sudo ./vagrant_setup.sh
-#set your_email@exmaple.com to your github email account.
-git config --global user.email "your_email@example.com"
-```
+See [development](https://github.com/FrozenSquirrels/polyglot/wiki/Development-Environment-Setup).
 
-You can now edit file via windows via the the shared directory:
- *  http://docs.vagrantup.com/v2/synced-folders/
-
-or in Linux with your favorit editor.
--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-#apt-get update
+apt-get update
 
 function set_git_config_commit_vim {
   git config --global core.editor "vim"
 }
 
 function install {
+  echo "INSTALLING: $1"
   apt-get -y install $1 || { echo "ERROR: installing ${1}"; exit 1; }
 }
 
 function install_node_package_manager {
-  install npm
+  if [ ! -e /usr/bin/npm ]; then
+    install npm
+  fi
   #Switch to http version (as apposed to https) due to proxy auth issues.
   #https://github.com/npm/npm/issues/2119
   npm config set registry http://registry.npmjs.org/
   npm install npm -g.
+}
+
+#upgrade nodejs
+function install_nodejs {
+  curl -sL https://deb.nodesource.com/setup | sudo bash -
+  apt-get install -y nodejs
 }
 
 
@@ -35,5 +44,12 @@ install python-sqlalchemy
 install sqlite3
 install libsqlite3-dev
 
+
+install curl
+install_nodejs
+
 install_node_package_manager
 
+install xvfb
+pip install selenium
+install chromium-browser
