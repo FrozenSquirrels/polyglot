b3dce90001bc68a837671b010a1ea76013ca0d84
Nathan Breitsch <nathanbreitsch@gmail.com>
2015-04-13 14:18:38 -0400

    navbar update, project section

---
+++ server/project.py
@@ -0,0 +1,163 @@
+from bottle import route, run, debug, template, request, post, get, static_file
+import bottle
+import pymongo
+import csv
+import sys
+import tempfile
+
+import os
+import json
+import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, BasicPrompt
+
+from pymongo import MongoClient
+from pymongo import DESCENDING
+from bson.objectid import ObjectId
+
+#bottle should look in /static/views for templates
+bottle.TEMPLATE_PATH.insert(0,'../static/views')
+
+#client = MongoClient('localhost', 8080)
+client = pymongo.MongoClient()
+db = client.project
+collection = db.word
+
+#root to landing page (same as /project)
+@route('/')
+def index():
+    return template('index')
+
+#html file should go in views
+@get('/<filepath:re:.*\.html>')
+def static_views(filepath):
+    return static('views/'+filepath)
+
+
+
+#serve third party javascript and css in bower_components folder
+@get('/bower_components/<filepath:path>')
+def bower_files(filepath):
+    return static_file(filepath, root='../bower_components')
+
+#serve static javascript and html
+@get('/static/<filepath:path>')
+def static(filepath):
+    return static_file(filepath, root='../static')
+
+#get request for get promts, will replace with mongo code
+@get('/getBasicPrompts')
+def getPrompt():
+    #construct database session
+    engine = create_engine('sqlite:///polyglot.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind = engine)
+    session = DBSession()
+    results = session.query(BasicPrompt).all()
+    print(results)
+    return json.dumps([x.serialize for x in results])
+    session.close()
+
+#practice section
+@route('/practice')
+def practice_page():
+    return static_file("practice.html", root="../static/views")
+
+######## the home page (first screen )
+
+@route('/project')
+def index_page():
+	return template('index')
+
+  #####################################
+
+    # To Enter the Admin page
+@post('/login')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+
+    items = db.word.find().sort("English", pymongo.DESCENDING)
+    R = []
+    for item in items:
+        R.append((item["_id"],item["English"],item["Hindi"]))
+    if  username == "Hanan" and password == "hhh" or username == "Reem" and password == "rrr" or username == "Nathan" and password == "nnn" :
+        return template('Admin', row=R, name=username)
+    else:
+        return template('fail')
+
+@route('/login')
+def LOG():
+    return template('Admin')
+
+
+
+    ############################## EDIT GET ###################################
+
+@route('/project/edit/<_id>', method='GET')
+def get_edit(_id):
+    item = db.work.find_one({ "_id":ObjectId(_id)})
+    return template('edit',r1=item["English"],r2=item["Hindi"], item=_id)
+    #------------------------------ EDIT POST ---------------------------------------------
+
+@route('/login/insert', method='POST')
+def post_new_item():
+    print "IN ITEM POST"
+
+    English = request.POST.get('English', '').strip()
+    Hindi = request.POST.get('Hindi', '').strip()
+
+
+    item["English"] = English
+    item["Hindi"] = Hindi
+
+
+    db.word.save(item)
+
+    return template('success',item=_id)
+
+
+############################## INSERT NEW words ###################################
+
+@route('/login/insert', method='GET')
+def get_new_item():
+    return template('ADD_Word.tpl')
+
+@route('/login/insert', method='POST')
+def post_new_item():
+    print "IN ITEM POST"
+
+    English = request.POST.get('English', '').strip()
+    Hindi = request.POST.get('Hindi', '').strip()
+
+
+
+    db.word.insert({"English":English, "Hindi":Hindi})
+    return template('Add_success', C=Cereal)
+    commit()
+
+
+        ############################## DELETE ###################################
+
+
+@route('/project/delete/<_id>', method='GET')
+def delete(_id):
+    item = db.word.find_one({ "_id":ObjectId(_id)})
+    return template('delete',r1=item["English"],r2=item["Hindi"])
+
+
+@route('/project/delete/<_id>', method='POST')
+def delete(_id):
+
+    item = db.word.find_one({ "_id":ObjectId(_id)})
+    English = request.POST.get('English','').strip()
+    db.word.remove(item)
+    return template('success',item=_id)
+
+#----------------------------------------------
+
+
+
+debug(True)
+run(host='localhost',port=8080,reloader=True)
---
+++ static/views/index.tpl
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+*{
+     padding : 0;
+     margin : 0;
+     border : 0;
+}
+body{
+    background-color:#666666;
+
+
+     background-attachment : scroll;
+     background-size : 100% auto;
+
+}
+.blended_grid{
+     display : block;
+     width : 900px;
+     overflow : auto;
+     margin : 30px auto 0 auto;
+}
+.pageHeader{
+
+     background-image : url( 'http://www.willamette.edu/dept/llc/images/headers/01.gif');
+     background-repeat: no-repeat;
+     background-size: 900px 200px;
+
+
+     float : left;
+     clear : none;
+     height : 200px;
+     width : 1000px;
+}
+.nav {
+
+      background-color :  #333333;
+
+     float : left;
+     clear : none;
+     height : 55px;
+     width : 900px;
+     border: 6px;
+}
+.pageContent{
+     background-color :     #CCCC9A;
+    background-image: url("image.png");
+    background-repeat: no-repeat;
+
+     float : left;
+     clear : none;
+     height : 500px;
+     width : 900px;
+     padding-left: 10px;
+
+}
+.navMENU{
+
+     background-color : rgba(254,247,255,0.8);
+     height : 500px;
+}
+.navMENU ul {
+
+}
+.navMENU ul li {
+     list-style-type :none;
+      width : 222px;
+      float : left;
+      border-right: 2px solid #ccc;
+      text-align: center;
+}
+.navMENU li a {
+     text-denoration: none;
+     color: black;
+     line-height: 50px;
+     display: block;
+
+}
+.navMENU li a:hover {
+    background-color: #990033;
+}
+
+
+
+.pageFooter{
+
+     background-color : #333333;
+
+     float : left;
+     clear : none;
+     height : 100px;
+     width : 900px;
+
+
+}
+
+.image {
+   content:url(c1.jpg);
+}​
+
+
+
+</style>
+
+
+
+
+
+ <title>Home</title>
+     <meta name="description" content="* WELCOME TO The LEARNING WEBSITE *">
+
+</head>
+<body>
+
+<div class="blended_grid">
+      <div class="pageHeader">
+        <br><br>
+                <h1 align="center" style="font-size:35px;text-shadow:3px 3px 3px #666;color:#6B008F;">   </h1>
+
+
+        <br><br>
+
+      </div>
+
+<div class="nav">
+<br>
+
+        <form action="/login" method="post" align="center" color: white>
+
+            <b> Admin Username:</b> <input name="username" type="text"  />
+            <b>Admin Password:</b> <input name="password" type="password" />
+
+            <input value="Reset" type="reset" style="color:#111111;border-radius:4px; width:80px; height:20px; background-color:#FFFFFF;"/>
+            <input value="Login" type="submit" style="color:#111111;border-radius:4px; width:80px; height:20px; background-color:#FFFFFF;"/>
+
+        </form>
+</div>
+<!------------------------------------>
+
+
+      <div class="pageLeftMenu">
+
+      </div>
+<!------------------------------------>
+
+
+
+
+<div class = "navMENU" >
+<ul>
+    <li><a href="#"> About Us</a></li>
+
+    <li><a href="http://localhost:8080/project/dataset" > Guest Enter</a></li>
+    <li><a href="#"> Contact Us</a></li>
+    <li><a href="/practice"> Practice </a></li>
+
+
+</ul>
+
+<script src="Script.js"></script>
+
+
+
+<!------------------------------------>
+
+
+
+      <div class="pageContent">
+        <br><br>
+
+
+
+
+         <div align="center" >
+<FONT face="Arial ,Tahoma" size="5" color="#FFFF66">
+<marquee bgcolor="#CCCC9A;" direction="right">Learn New Language: New Years Resolutions  </marquee>
+</FONT>
+
+           <img src="http://dirtyclassroom.com/wp-content/uploads/2013/10/Squirrel-8.jpg">
+         </div>
+
+
+
+
+
+      </div>
+
+<!------------------------------------>
+
+
+
+
+
+      <div class="pageFooter">
+        <br>
+      <h4 align="center" style= color:white> For Software Engineering Methodology Course </h4>
+
+      <h4 align="center" style= color:white> Developed by Frozen Squirrels  </h4>
+
+      <h4 align="center" style= color:white> © All right reserved   </h4>
+      </div>
+
+</body>
+</html>
