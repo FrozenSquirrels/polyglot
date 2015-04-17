c72cac4fd671914bebc9e838becc374dc2803587
rsuhaibani <ralsuhai@kent.edu>
2015-04-17 19:42:29 -0400

    Unit Testing for the login using Chrome browser

--- UnitTest.py
+++ UnitTest.py
@@ -1,6 +1,4 @@
 from selenium import webdriver 
-#import unittest
-# from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
@@ -9,6 +7,8 @@ driver.get("http://localhost:8080/project")
 
 driver.find_element_by_id("username").send_keys("Reem")
 driver.find_element_by_id("password").send_keys("rrr\n")
+
 time.sleep(30)
+
 driver.close()
 driver.quit()
