3394770f72180fec9df732f5d3687480ca026ecc
hananmuhajab <hmuhajab@kent.edu>
2015-04-18 22:16:59 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,148 @@
+from bottle import route, run, debug, template, request, post, get
+import pymongo
+import csv
+import sys
+import tempfile
+
+from pymongo import MongoClient
+from pymongo import DESCENDING
+from bson.objectid import ObjectId
+
+#client = MongoClient('localhost', 8080)
+client = pymongo.MongoClient()
+db = client.project
+collection = db.word 
+
+######## the home pagge (first screen )
+ 
+@route('/project')
+def index_page():
+	return template('index')
+
+  #####################################
+
+    # To Enter the Admin page
+@post('/login') 
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+
+    items = db.word.find().sort("English", pymongo.DESCENDING)
+    R = []
+    for item in items:
+        R.append((item["_id"],item["English"],item["Hindi"]))
+    if  username == "Hanan" and password == "hhh" or username == "Reem" and password == "rrr" or username == "Nathan" and password == "nnn" :
+        return template('Admin', row=R, name=username)
+    else:
+        return template('fail')
+
+@route('/login')
+def LOG():
+    return template('Admin')
+
+
+############## for guest #######################
+
+
+@route('/project/dataset')
+def Word():
+
+    items = db.word.find().sort("English", pymongo.DESCENDING)
+    rows = []
+    for item in items:
+        rows.append((item["English"],item["Hindi"]))
+    return template('guest', row=rows)
+    commit()
+
+
+
+############## for about us #######################
+
+
+@route('/project/aboutus')
+def about_us():
+
+
+    return template('about')
+    commit()
+
+
+    ############## for contact us #######################
+
+
+@route('/project/contactus')
+def contact_us():
+
+
+    return template('contact')
+    commit()
+
+    ############################## EDIT GET  not yet ###################################
+
+@route('/project/edit/<_id>', method='GET')
+def get_edit(_id):
+    item = db.work.find_one({ "_id":ObjectId(_id)})
+    return template('edit',r1=item["English"],r2=item["Hindi"], item=_id)
+    #------------------------------ EDIT POST ---------------------------------------------
+
+@route('/login/insert', method='POST')
+def post_new_item():
+    print "IN ITEM POST"
+                                                        
+    English = request.POST.get('English', '').strip()
+    Hindi = request.POST.get('Hindi', '').strip()
+   
+
+    item["English"] = English
+    item["Hindi"] = Hindi
+   
+
+    db.word.save(item)
+
+    return template('success',item=_id)
+
+
+############################## INSERT NEW Cereal ###################################
+
+@route('/login/insert', method='GET')
+def get_new_item():
+    return template('insert.tpl')
+
+@route('/login/insert', method='POST')
+def post_new_item():
+    print "IN ITEM POST"
+                                                        
+    English = request.POST.get('English', '').strip()
+    Hindi = request.POST.get('Hindi', '').strip()
+    
+    
+
+    db.word.insert({"English":English, "Hindi":Hindi})
+    return template('Add_success', C=English)
+    commit()
+
+
+        ############################## DELETE not yet ###################################
+
+
+@route('/project/delete/<_id>', method='GET')
+def delete(_id):
+    item = db.word.find_one({ "_id":ObjectId(_id)})
+    return template('delete',r1=item["English"],r2=item["Hindi"])
+
+
+@route('/project/delete/<_id>', method='POST')
+def delete(_id):
+
+    item = db.word.find_one({ "_id":ObjectId(_id)})
+    English = request.POST.get('English','').strip()
+    db.word.remove(item)
+    return template('success',item=_id)
+
+#----------------------------------------------
+
+
+
+debug(True)
+run(host='localhost',port=8080,reloader=True)
+
